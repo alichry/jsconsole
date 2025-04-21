ed65b8cfc26a8ce3d704b9bb9b5e22b7bdf6b2b3
Ali Cherry <32630359+alichry@users.noreply.github.com>
2025-04-21 15:08:43 +0100

    add default theme prop impl

--- src/core/Root.js
+++ src/core/Root.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './containers/App';
+import PropTypes from 'prop-types';
+import { setTheme } from './actions/Settings';
 
-export default function Root(appProps) {
+export default function Root({ defaultTheme }) {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    const run = () => {
+      if (! defaultTheme) {
+        return;
+      }
+      const state = store.getState();
+      if (! state.settings.theme) {
+        store.dispatch(setTheme(defaultTheme));
+      }
+    };
+    run();
+    setReady(true);
+  }, []);
+  if (! ready) {
+    return <></>;
+  }
   return (
     <Provider store={store}>
-      <App {...appProps} />
+      <App />
     </Provider>
   );
 }
+
+Root.propTypes = {
+  defaultTheme: PropTypes.oneOf(['light', 'dark'])
+};
